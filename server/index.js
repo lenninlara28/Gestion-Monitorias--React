ee5320821d4827572d6be23b1346fabc34a585d5
LenninLara <lenninlara28@hotmail.com>
2020-10-23 14:37:37 -0500

    delete, falta editar y VALIDACIONES

--- server/index.js
+++ server/index.js
@@ -41,6 +41,51 @@ app.get("/api/monitores/get", (req, res) => {
     });
 });
 
+//API para Eliminar
+app.delete("/api/delete/:Monitor", (req, res) => {
+    const idMonitor = req.params.Monitor
+    
+    const sqlDelete =
+        "DELETE FROM monitores WHERE idMonitor = (?)";
+        db.query(sqlDelete, [idMonitor], (err, result) => {
+        if (err) console.log(err);
+    });
+});
+
+//API para Insert
+app.post("/api/monitorias/insert", (req, res) => {
+    const materia = req.body.materia
+    const idMonitor = req.body.idMonitor
+    const fecha = req.body.fecha
+    const salon = req.body.salon
+
+    const sqlInsert =
+        "INSERT INTO monitorias (idMonitor,Materia,Fecha,Salon) VALUES (?,?,?,?)";
+        db.query(sqlInsert, [idMonitor, materia, fecha, salon], (err, result) => {
+        console.log(err);
+    });
+});
+
+//API para Traer
+app.get("/api/monitorias/get", (req, res) => {
+    const sqlSelect =
+        "SELECT * FROM monitorias";
+        db.query(sqlSelect,(err, result) => {
+        res.send(result);
+    });
+});
+
+//API para Eliminar
+app.delete("/api/delete/monitoria/:Monitoria", (req, res) => {
+    const idMonitoria = req.params.Monitoria
+    
+    const sqlDelete =
+        "DELETE FROM monitorias WHERE idMonitorias = (?)";
+        db.query(sqlDelete, [idMonitoria], (err, result) => {
+        console.log(err);
+    });
+});
+
 app.listen(3001, () => {
     console.log("Running on port 3001")
 })
